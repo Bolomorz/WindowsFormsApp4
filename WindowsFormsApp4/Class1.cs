17762e05cead5d7c9c59883ebbef94092f7c0048
Dominik Schneider <Dominik.Schneider@wibizu.de>
2023-01-27 10:35:26 +0100

    Compare

--- WindowsFormsApp4/Class1.cs
+++ WindowsFormsApp4/Class1.cs
@@ -9,6 +9,7 @@ namespace WindowsFormsApp4
     public class YSeries
     {
         public List<double> values = new List<double>();
+        public double min, max, interval, val;
         public string name;
         public System.Drawing.Color color;
 
@@ -17,11 +18,50 @@ namespace WindowsFormsApp4
             name = sinput;
             color = cinput;
             values.Clear();
+            min = double.MaxValue;
+            max = double.MinValue;
+            val = 1;
         }
 
         public void Add(double input)
         {
             values.Add(input);
+        }
+
+        private void CompareVal(double input)
+        {
+            if(input > 0)
+            {
+                while(input > val)
+                {
+                    val = val * 10;
+                }
+            }
+            else
+            {
+                while(input * -1 > val)
+                {
+                    val = val * 10;
+                }
+            }
+        }
+        private void CompareMax(double input)
+        {
+            if(input > max)
+            {
+                double div = input / (val / 10);
+                double idiv = Math.Floor(div);
+                max = val * idiv;
+            }
+        }
+        private void CompareMin(double input)
+        {
+            if(input < min)
+            {
+                double div = input / (val / 10);
+                double idiv = Math.Floor(div);
+                min = val * idiv;
+            }
         }
     }
 }
