24fcbbf42b70380f320864540e9f038c1e176b1c
Dominik Schneider <Dominik.Schneider@wibizu.de>
2023-01-27 10:35:26 +0100

    ReadData FunctionCompare

--- WindowsFormsApp4/Class1.cs
+++ WindowsFormsApp4/Class1.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WindowsFormsApp4
 {
     public class YSeries
     {
         public List<double> values = new List<double>();
+        public double min, max, interval;
         public string name;
         public System.Drawing.Color color;
 
@@ -17,11 +19,117 @@ namespace WindowsFormsApp4
             name = sinput;
             color = cinput;
             values.Clear();
+            min = int.MaxValue;
+            max = int.MinValue;
         }
 
         public void Add(double input)
         {
+            //Add value <input> to List
             values.Add(input);
+            CompareMax(input);
+            CompareMin(input);
+            interval = (max - min) / 10;
+        }
+
+        private void CompareMax(double input)
+        {
+            //
+            //string output = Environment.NewLine + "CompareMax(" + input + ")" + max + Environment.NewLine;
+            //Compare <input> with current maxValue, change maxValue if necessary
+            if(input > max)
+            {
+                //output += "input > max = true" + Environment.NewLine;
+                int val = 1;
+
+                //-
+                //Find position of <value>, f.e. input = 345 => val = 100;
+                while(input > val)
+                {
+                    //output += "input: " + input + " val: " + val + Environment.NewLine;
+                    val = val * 10;
+                }
+                val = val / 10;
+                //output += val + Environment.NewLine;
+                //-
+
+                //-
+                //Find new maxvalue from <value>, f.e. input = 345 => max = 400;
+                max = 0;
+                while(input > max)
+                {
+                    //output += "max: " + max + " val: " + val + Environment.NewLine;
+                    max = max + val;
+                }
+                //-
+            }
+            else
+            {
+                //output += "input > max = false" + Environment.NewLine;
+            }
+            //File.AppendAllText(@"C:\Users\dominik.schneider\Documents\test.txt", output);
+        }
+
+        private void CompareMin(double input)
+        {
+            //string output = Environment.NewLine + "CompareMin(" + input + ")" + min + Environment.NewLine;
+            //Compare <input> with current minValue, change minValue if necessary
+            if (input < min)
+            {
+                //output += "input < min = true" + Environment.NewLine; 
+                int val;
+                if(input < 0)
+                {
+                    //output += "input < 0 = true" + Environment.NewLine;
+                    val = -1;
+                    while (input < val)
+                    {
+                        //output += "input: " + input + " val: " + val + Environment.NewLine;
+                        val = val * 10;
+                    }
+                    val = val / 10;
+                    //output += "val: " + val + Environment.NewLine;
+                    min = 0;
+                    while(input < min)
+                    {
+                        //output += "input: " + input + " min: " + min + Environment.NewLine;
+                        min = min + val;
+                    }
+                    //output += "min: " + min + Environment.NewLine;
+                    if((-1) * min < max / 10)
+                    {
+                        min = (-1) * (max / 10);
+                    }
+                }
+                else
+                {
+                    //output += "input < 0 = false" + Environment.NewLine;
+                    val = 1;
+                    while (input > val)
+                    {
+                        //output += "input: " + input + " val: " + val + Environment.NewLine;
+                        val = val * 10;
+                    }
+                    val = val / 10;
+                    //output += " val: " + val + Environment.NewLine;
+                    min = 0;
+                    while(input > min + val)
+                    {
+                        //output += "input: " + input + " min: " + min + Environment.NewLine;
+                        min = min + val;
+                    }
+                    //output += "min: " + min + Environment.NewLine;
+                    if(min < max / 10)
+                    {
+                        min = 0;
+                    }
+                }
+            }
+            else
+            {
+                //output += "input < min = false" + Environment.NewLine;
+            }
+            //File.AppendAllText(@"C:\Users\dominik.schneider\Documents\test.txt", output);
         }
     }
 }
--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 using cc = System.Windows.Forms.DataVisualization.Charting;
 
@@ -14,16 +15,44 @@ namespace WindowsFormsApp4
 {
     public partial class Form1 : Form
     {
+        //-
+        //List of Values for each Series
+        //For each xval (x-coordinate) one yval (y-coordinate) in each Series
+        //for each xval[i] one yval[j].values[i] for each yval[j]
         public List<double> xval = new List<double>();
         public List<YSeries> yval = new List<YSeries>();
+        //-
 
-        public int minx, maxx;
+        //-
+        //Values x1 and x2 as Intervall which will be shown in chart [x1, x2]
+        //Can later be Set through 2 TextBoxes
         public int x1, x2;
+        //-
 
+        //-
+        //Wether a grid will be shown in chart or not
+        //Can later be Set by a CheckBox
         public bool gridEnabled;
+        //-
 
+        //-
+        //Title of the x-Axis
+        //Will be read out of file
         public string xvalname;
+        //-
+
+        //-
+        //Title of chart; current Date
+        //Title can be set through TextBox; Date will be calculated dynamically
         public string chartTitle, chartDate;
+        //-
+
+        //-
+        //path of file to read
+        //can be set with OpenFileDialog through ButtonClick
+        public string filepath;
+        //-
+
         public Form1()
         {
             InitializeComponent();
@@ -31,16 +60,73 @@ namespace WindowsFormsApp4
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Set Variables on FormLoad
             gridEnabled = false;
             chartDate = DateTime.Now.ToString("d");
+            filepath = "";
+            chartTitle = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog on ButtonClickEvent to select file to open
+            
+            string path = "";
+
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Title = "Open File Dialog";
+            fd.InitialDirectory = @"C:\Users\dominik.schneider\Documents\";
+            fd.Filter = "All files (*.txt) | *.txt";
+            fd.FilterIndex = 1;
+            fd.Multiselect = false;
+            fd.RestoreDirectory = true;
+
+            if(fd.ShowDialog() == DialogResult.OK)
+            {
+                path = fd.FileName;
+            }
+
+            //-
+            //Show opened file in TextBox
+            filepath = path;
+            textBox1.Text = path;
+            //-
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //-
+            //Text in textbox can only be overwritten by OpenFileDialog on ButtonClickEvent, cannot be manually overwritten
+            textBox1.Text = filepath;
+            //-
+
+            //-
+            //if Text changed from OpenFileDialog -> read data from file, draw chart
+            ReadData();
+            DrawChart();
+            //-
         }
 
         private void CreateYAxis(
             cc.Chart chart, 
             cc.ChartArea area,
-            cc.Series series,
+            YSeries yseries,
             float axisOffset, float labelsSize)
         {
+            cc.Series series = new cc.Series();
+            series.Name = yseries.name;
+            for (int i = 0; i < yseries.values.Count(); i++)
+            {
+                if (xval.ElementAt(i) >= x1 && xval.ElementAt(i) <= x2)
+                {
+                    series.Points.AddXY(xval.ElementAt(i), yseries.values.ElementAt(i));
+                }
+            }
+            series.Color = yseries.color;
+            series.ChartType = cc.SeriesChartType.Line;
+            series.BorderWidth = 2;
+            chart.Series.Add(series);
+
             cc.ChartArea areaSeries = chart.ChartAreas.Add("ChartArea_" + series.Name);
             areaSeries.BackColor = Color.Transparent;
             areaSeries.BorderColor = Color.Transparent;
@@ -57,7 +143,9 @@ namespace WindowsFormsApp4
             areaSeries.AxisY.LabelStyle.Enabled = false;
             areaSeries.AxisY.IsStartedFromZero = area.AxisY.IsStartedFromZero;
 
-            //TODO AXISY.MAX/MIN
+            areaSeries.AxisY.Minimum = yseries.min;
+            areaSeries.AxisY.Maximum = yseries.max;
+            areaSeries.AxisY.Interval = yseries.interval;
 
             series.ChartArea = areaSeries.Name;
 
@@ -87,7 +175,13 @@ namespace WindowsFormsApp4
             areaAxis.AxisY.IsStartedFromZero = area.AxisY.IsStartedFromZero;
             areaAxis.AxisY.LabelStyle.Font = area.AxisY.LabelStyle.Font;
 
-            //TODO AXISX.MAX/MIN, AXISY.MAX/MIN
+            areaAxis.AxisX.Minimum = area.AxisX.Minimum;
+            areaAxis.AxisX.Maximum = area.AxisX.Maximum;
+            areaAxis.AxisX.Interval = area.AxisX.Interval;
+
+            areaAxis.AxisY.Minimum = yseries.min;
+            areaAxis.AxisY.Maximum = yseries.max;
+            areaAxis.AxisY.Interval = yseries.interval;
 
             areaAxis.Position.X = axisOffset;
             areaAxis.InnerPlotPosition.X = areaAxis.InnerPlotPosition.X + labelsSize;
@@ -173,37 +267,146 @@ namespace WindowsFormsApp4
             }
             chart1.Series.Add(xserie);
 
-            foreach(YSeries y in yval)
-            {
-                cc.Series si = new cc.Series();
-                si.Name = y.name;
-                for(int i = 0; i < y.values.Count(); i++)
-                {
-                    if(xval.ElementAt(i) >= x1 && xval.ElementAt(i) <= x2)
-                    {
-                        si.Points.AddXY(xval.ElementAt(i), y.values.ElementAt(i));
-                    }
-                }
-                si.Color = y.color;
-                si.ChartType = cc.SeriesChartType.Line;
-                si.BorderWidth = 2;
-                chart1.Series.Add(si);
-            }
-
             float offset = 0;
-            for(int i = 1; i <= chart1.Series.Count(); i++)
+            foreach(YSeries yseries in yval)
             {
-                CreateYAxis(chart1, chart1.ChartAreas.FindByName("Default"), chart1.Series.ElementAt(i), offset, 4);
+                CreateYAxis(chart1, chart1.ChartAreas.FindByName("Default"), yseries, offset, 4);
                 offset += 4;
             }
 
             if(xval.Count > 0)
             {
-                for(int i = 1; i < chart1.Series.Count(); i++)
+                foreach(YSeries yseries in yval)
                 {
-                    CreateDataPoints(chart1.Series.ElementAt(i));
+                    CreateDataPoints(chart1.Series.FindByName(yseries.name));
                 }
             }
         }
+
+        private void ReadData()
+        {
+            //read data from file
+
+            //-
+            //value of last x added
+            //if value of new x <= lastx dont add to Series, so it always creates a steady function, not a relation
+            //=> x is steadily increasing in displayed function (not decreasing or staying constant)
+            double lastx = double.MinValue;
+            //-
+
+            if(System.IO.File.Exists(filepath))
+            {
+                //if file in filepath exists, read each line of file for input as data
+                //1. line of file:<xAxisName> <1.SeriesName> <2.SeriesName> ... <n.SeriesName>
+                //m. line of file:<m.XValue> <m.YValOf1.Series> ... <m.YValOfn.Series>
+                
+                //-
+                //Clear Lists
+                xval.Clear();
+                yval.Clear();
+                //-
+
+                //-
+                //count counting lines in file
+                int count = 0;
+                //-
+
+                foreach (string line in System.IO.File.ReadLines(filepath))
+                {
+                    //-
+                    //foreach line in file, split line at ' '
+                    string[] strlist = line.Split(' ');
+                    //-
+
+                    if (count == 0)
+                    {
+                        //count = 0 => first line
+
+                        //-
+                        //count2 counting items in strlist
+                        int count2 = 0;
+                        //-
+
+                        foreach(string str in strlist)
+                        {
+                            if(count2 == 0)
+                            {
+                                //first item of strlist => name of xAxis
+                                xvalname = str;
+                            }
+                            else
+                            {
+                                //n. item of strlist => new YSeries with name of Series and Color
+                                //currently 8 YSeries supported for different Colors
+                                switch(count2)
+                                {
+                                    case 1: yval.Add(new YSeries(str, Color.Red)); break;
+                                    case 2: yval.Add(new YSeries(str, Color.Blue)); break;
+                                    case 3: yval.Add(new YSeries(str, Color.Green)); break;
+                                    case 4: yval.Add(new YSeries(str, Color.Orange)); break;
+                                    case 5: yval.Add(new YSeries(str, Color.Brown)); break;
+                                    case 6: yval.Add(new YSeries(str, Color.Yellow)); break;
+                                    case 7: yval.Add(new YSeries(str, Color.Turquoise)); break;
+                                    case 8: yval.Add(new YSeries(str, Color.Black)); break;
+                                    default: yval.Add(new YSeries(str, Color.Gray)); break;
+                                }
+                            }
+                            count2++;
+                        }
+                    }
+                    else
+                    {
+                        //-
+                        //count2 counting items in strlist
+                        int count2 = 0;
+                        //-
+
+                        //-
+                        //x value of current line
+                        double x = Convert.ToDouble(strlist[0]);
+                        //-
+
+                        foreach(string str in strlist)
+                        {
+                            if(count2 == 0 && x > lastx)
+                            {
+                                //-
+                                //first value of currentline => value of x
+                                //add only if current x > lastx
+                                xval.Add(Convert.ToDouble(str));
+                                //-
+                            }
+                            else if(x > lastx)
+                            {
+                                //-
+                                //n. value of currentline => y value of n-1. Series
+                                yval[count2 - 1].values.Add(Convert.ToDouble(str));
+                                //-
+                            }
+                            count2++;
+                        }
+                        if(x > lastx)
+                        {
+                            //-
+                            //set lastx if x > lastx
+                            lastx = x;
+                            //-
+                        }
+                    }
+                    count++;
+                }
+
+                //-
+                //for now for testing x values in interval [0, 10]
+                //-----
+                //TODO 
+                //set dynamically 
+                x1 = 0;
+                x2 = 10;
+                //-
+            }
+
+        }
+
     }
 }
